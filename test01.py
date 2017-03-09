13c99efa652a9ec73480f28e0a9336c2fab342c6
xingxingxingshiwo <1509354170@qq.com>
2017-03-10 00:11:40 +0800

    310

--- test01.py
+++ test01.py
@@ -1,2 +1,23 @@
 # coding = utf-8 
-print ('hello,world!')
+class Programmer(object):
+	"""docstring for ClassName"""
+	def __init__(self, name,age,):
+		self._age= age
+		self.name = name
+	def function(self):
+		print"%s is %s"%(self.name,self._age)
+
+
+class BrokenProgrammer(Programmer):
+	"""docstring for ClassName"""
+	def __init__(self, name,age):
+		super(BrokenProgrammer, self).__init__(name)
+		self.arg = arg
+		self._name = name
+
+if __name__ == '__main__':
+	programmer = Programmer('xingxing',18)
+	programmer.function()
+
+
+		
